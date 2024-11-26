ac39ce4e158a74b94f2b97110b24bf467c377b04
Winniemoraes <moraesninny@gmail.com>
2024-11-26 14:19:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Mitologia',
+    'Qual é o Titã que engole os próprios filhos?',
+    'Cronos'
+)
+
+criaCartao(
+    'Mitologia',
+    'Quem é o Deus do sol e das poesias?',
+    'Apollo'
+)
+
+criaCartao(
+    'Premiações',
+    'Qual é a pessoa com mais Oscars?',
+    'Walt Disney com 26 Oscars'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz goiaba em Inglês?',
+    'Goiaba em ingles é GUAVA (guava')
